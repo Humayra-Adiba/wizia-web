7574790ea85e1742a9f8b2693dbe421fb01e1506
Humayra-Adiba <tahsinadiba2024@outlook.com>
2024-12-03 19:27:25 +0600

    Card section done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card() {
   const CardData = [
     {
-      Img: "card1.svg",
+      Img: "card3.svg",
       Heading: "You’re in Control",
       Description:"aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work.",
     },
@@ -13,7 +13,7 @@ function Card() {
       Description:"Train an aiDR on a patch. When you are happy with the results, scale it effortlessly.",
     },
     {
-      Img: "card3.svg",
+      Img: "card1.svg",
       Heading: "Incredibly Flexible",
       Description:"Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time.",
     },
@@ -21,9 +21,32 @@ function Card() {
 
   return (
     <>
-      <section className=" bg-[#002228] text-white">
-        <div className="flex  flex-col sm:flex-row md:flex-row lg:flex-row ml-[20px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] gap-[24px] px-[24px]">
-          
+      <section className=" bg-[#002228] text-white py-[50px] px-[35px]">
+        <div className="flex  flex-col sm:flex-row md:flex-row lg:flex-row ml-[20px] sm:ml-[50px] md:ml-[120px] max-w-[1440px] gap-[24px] ">
+        {CardData.map((item, index) => (
+            <div
+              key={index}
+              className="sm:w-[384px] pt-8 "
+            >
+              <div>
+                <div>
+                  <img
+                    src={item.Img}
+                    alt="Logos"
+                    className="max-w-[54px] min-h-[48px]"
+                  />
+                </div>
+              </div>
+              <div>
+                <h3 className="text-[28px] font-semibold mt-[20px]">
+                  {item.Heading}
+                </h3>
+                <p className="text-[18px] w-full md:w-[260px] lg:w-[260px] text-[#FFFFFF] mt-[10px]">
+                  {item.Description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </>
